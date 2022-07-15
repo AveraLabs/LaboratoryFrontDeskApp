f2a167ded41d789e2143862b3e4eb8cf4a9c0e9e
eben-siaw <edei-siaw@st.ug.edu.gh>
2022-07-15 15:34:55 +0100

    new

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../Components/Navbar'; 
 import Sidebar from '../Components/Sidebar';
-import {Link} from 'react-router-dom';
+
 
 function Home() {
     return (
@@ -9,248 +9,13 @@ function Home() {
       <div className="layout-wrapper layout-content-navbar">
         <div className="layout-container">
           {/* <!-- Menu --> */}
-  
-          <aside id="layout-menu" className="layout-menu menu-vertical menu bg-menu-theme">
-            <div className="app-brand demo">
-              <a href="index.html" className="app-brand-link">
-                <span className="app-brand-logo demo">
-                  <svg
-                    width="26px"
-                    height="26px"
-                    viewBox="0 0 26 26"
-                    version="1.1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    // xmlns:xlink="http://www.w3.org/1999/xlink"
-                  >
-                    <title>icon</title>
-                    <defs>
-                      <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="linearGradient-1">
-                        <stop stop-color="#5A8DEE" offset="0%"></stop>
-                        <stop stop-color="#699AF9" offset="100%"></stop>
-                      </linearGradient>
-                      <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" id="linearGradient-2">
-                        <stop stop-color="#FDAC41" offset="0%"></stop>
-                        <stop stop-color="#E38100" offset="100%"></stop>
-                      </linearGradient>
-                    </defs>
-                    <g id="Pages" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                      <g id="Login---V2" transform="translate(-667.000000, -290.000000)">
-                        <g id="Login" transform="translate(519.000000, 244.000000)">
-                          <g id="Logo" transform="translate(148.000000, 42.000000)">
-                            <g id="icon" transform="translate(0.000000, 4.000000)">
-                              <path
-                                d="M13.8863636,4.72727273 C18.9447899,4.72727273 23.0454545,8.82793741 23.0454545,13.8863636 C23.0454545,18.9447899 18.9447899,23.0454545 13.8863636,23.0454545 C8.82793741,23.0454545 4.72727273,18.9447899 4.72727273,13.8863636 C4.72727273,13.5423509 4.74623858,13.2027679 4.78318172,12.8686032 L8.54810407,12.8689442 C8.48567157,13.19852 8.45300462,13.5386269 8.45300462,13.8863636 C8.45300462,16.887125 10.8856023,19.3197227 13.8863636,19.3197227 C16.887125,19.3197227 19.3197227,16.887125 19.3197227,13.8863636 C19.3197227,10.8856023 16.887125,8.45300462 13.8863636,8.45300462 C13.5386269,8.45300462 13.19852,8.48567157 12.8689442,8.54810407 L12.8686032,4.78318172 C13.2027679,4.74623858 13.5423509,4.72727273 13.8863636,4.72727273 Z"
-                                id="Combined-Shape"
-                                fill="#4880EA"
-                              ></path>
-                              <path
-                                d="M13.5909091,1.77272727 C20.4442608,1.77272727 26,7.19618701 26,13.8863636 C26,20.5765403 20.4442608,26 13.5909091,26 C6.73755742,26 1.18181818,20.5765403 1.18181818,13.8863636 C1.18181818,13.540626 1.19665566,13.1982714 1.22574292,12.8598734 L6.30410592,12.859962 C6.25499466,13.1951893 6.22958398,13.5378796 6.22958398,13.8863636 C6.22958398,17.8551125 9.52536149,21.0724191 13.5909091,21.0724191 C17.6564567,21.0724191 20.9522342,17.8551125 20.9522342,13.8863636 C20.9522342,9.91761479 17.6564567,6.70030817 13.5909091,6.70030817 C13.2336969,6.70030817 12.8824272,6.72514561 12.5388136,6.77314791 L12.5392575,1.81561642 C12.8859498,1.78721495 13.2366963,1.77272727 13.5909091,1.77272727 Z"
-                                id="Combined-Shape2"
-                                fill="url(#linearGradient-1)"
-                              ></path>
-                              <rect
-                                id="Rectangle"
-                                fill="url(#linearGradient-2)"
-                                x="0"
-                                y="0"
-                                width="7.68181818"
-                                height="7.68181818"
-                              ></rect>
-                            </g>
-                          </g>
-                        </g>
-                      </g>
-                    </g>
-                  </svg>
-                </span>
-                <span className="app-brand-text demo menu-text fw-bold ms-2">Avera Labs</span>
-              </a>
-  
-              <a href="javascript:void(0);" className="layout-menu-toggle menu-link text-large ms-auto">
-                <i className="bx menu-toggle-icon d-none d-xl-block fs-4 align-middle"></i>
-                <i className="bx bx-x d-block d-xl-none bx-sm align-middle"></i>
-              </a>
-            </div>
-  
-            <div className="menu-divider mt-0"></div>
-  
-            <div className="menu-inner-shadow"></div>
-  
-            <ul className="menu-inner py-1">
-              {/* <!-- Dashboards --> */}
-              <li className="menu-item active">
-                <a href="javascript:void(0);" className="menu-link">
-                  <i className="menu-icon tf-icons bx bx-home-circle"></i>
-                  <div data-i18n="Dashboards">Pending Bookings</div>
-                </a>
-                <ul className="menu-sub">
-                  <li className="menu-item">
-                    <a href="index.html" className="menu-link">
-                      <div data-i18n="Analytics">All tests today</div>
-                    </a>
-                  </li>
-                  {/* <li className="menu-item">
-                    <a href="dashboards-ecommerce.html" className="menu-link">
-                      <div data-i18n="eCommerce">eCommerce</div>
-                    </a>
-                  </li> */}
-                </ul>
-              </li>
-  
-              {/* <!-- Layouts --> */}
-              <li className="menu-item">
-                <a href="javascript:void(0);" className="menu-link">
-                  <i className="menu-icon tf-icons bx bx-layout"></i>
-                  <div data-i18n="Layouts">All tests today</div>
-                </a>
-  
-                <ul className="menu-sub">
-                  <li className="menu-item">
-                    <a href="layouts-collapsed-menu.html" className="menu-link">
-                      <div data-i18n="Collapsed menu">Collapsed menu</div>
-                    </a>
-                  </li>
-                  <li className="menu-item">
-                    <a href="layouts-content-navbar.html" className="menu-link">
-                      <div data-i18n="Content navbar">Content navbar</div>
-                    </a>
-                  </li>
-                  <li className="menu-item">
-                    <a href="layouts-content-navbar-with-sidebar.html" className="menu-link">
-                      <div data-i18n="Content nav + Sidebar">Content nav + Sidebar</div>
-                    </a>
-                  </li>
-                  <li className="menu-item">
-                    <a href="../horizontal-menu-template" className="menu-link" target="_blank">
-                      <div data-i18n="Horizontal">Horizontal</div>
-                    </a>
-                  </li>
-                  <li className="menu-item">
-                    <a href="layouts-without-menu.html" className="menu-link">
-                      <div data-i18n="Without menu">Without menu</div>
-                    </a>
-                  </li>
-                  <li className="menu-item">
-                    <a href="layouts-without-navbar.html" className="menu-link">
-                      <div data-i18n="Without navbar">Without navbar</div>
-                    </a>
-                  </li>
-                  <li className="menu-item">
-                    <a href="layouts-fluid.html" className="menu-link">
-                      <div data-i18n="Fluid">Fluid</div>
-                    </a>
-                  </li>
-                  <li className="menu-item">
-                    <a href="layouts-container.html" className="menu-link">
-                      <div data-i18n="Container">Container</div>
-                    </a>
-                  </li>
-                  <li className="menu-item">
-                    <a href="layouts-blank.html" className="menu-link">
-                      <div data-i18n="Blank">Blank</div>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-  
-              {/* <!-- Apps & Pages --> */}
-              <li className="menu-header small text-uppercase"><span className="menu-header-text">OTHER SESSIONS</span></li>
-              <li className="menu-item">
-                <a href="app-calendar.html" className="menu-link">
-                  <i className="menu-icon tf-icons bx bx-calendar"></i>
-                  <div data-i18n="Calendar">Did not show up</div>
-                </a>
-              </li>
-              <li className="menu-item">
-                <a href="javascript:void(0);" className="menu-link">
-                  <i className="menu-icon tf-icons bx bx-food-menu"></i>
-                  <div data-i18n="Invoice">Cancelled appointment</div>
-                </a>
-                <ul className="menu-sub">
-                  <li className="menu-item">
-                    <a href="app-invoice-list.html" className="menu-link">
-                      <div data-i18n="List">List</div>
-                    </a>
-                  </li>
-                  <li className="menu-item">
-                    <a href="app-invoice-preview.html" className="menu-link">
-                      <div data-i18n="Preview">Preview</div>
-                    </a>
-                  </li>
-                  <li className="menu-item">
-                    <a href="app-invoice-edit.html" className="menu-link">
-                      <div data-i18n="Edit">Edit</div>
-                    </a>
-                  </li>
-                  <li className="menu-item">
-                    <a href="app-invoice-add.html" className="menu-link">
-                      <div data-i18n="Add">Add</div>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="menu-item">
-                <a href="javascript:void(0);" className="menu-link">
-                  <i className="menu-icon tf-icons bx bx-user"></i>
-                  <div data-i18n="Users">Calendar</div>
-                </a>
-                <ul className="menu-sub">
-                  <li className="menu-item active">
-                    <a href="app-user-list.html" className="menu-link">
-                      <div data-i18n="List">List</div>
-                    </a>
-                  </li>
-                  <li className="menu-item">
-                    <a href="javascript:void(0);" className="menu-link menu-toggle">
-                      <div data-i18n="View">View</div>
-                    </a>
-                    <ul className="menu-sub">
-                      <li className="menu-item">
-                        <a href="app-user-view-account.html" className="menu-link">
-                          <div data-i18n="Account">Account</div>
-                        </a>
-                      </li>
-                      <li className="menu-item">
-                        <a href="app-user-view-security.html" className="menu-link">
-                          <div data-i18n="Security">Security</div>
-                        </a>
-                      </li>
-                      <li className="menu-item">
-                        <a href="app-user-view-billing.html" className="menu-link">
-                          <div data-i18n="Billing & Plans">Billing & Plans</div>
-                        </a>
-                      </li>
-                      <li className="menu-item">
-                        <a href="app-user-view-notifications.html" className="menu-link">
-                          <div data-i18n="Notifications">Notifications</div>
-                        </a>
-                      </li>
-                      <li className="menu-item">
-                        <a href="app-user-view-connections.html" className="menu-link">
-                          <div data-i18n="Connections">Connections</div>
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-        
-              <li className="menu-item">
-               
-                <ul className="menu-sub">
-                
-                </ul>
-              </li>
-            </ul>
-          </aside>
-  
+              <Sidebar /> 
+
           {/* <!-- Layout container --> */}
           <div className="layout-page">
-           
             {/* <!-- Navbar --> */}
-           
-              <Navbar />   
-                  
+              <Navbar />      
+
             {/* <!-- / Navbar --> */}
   
             {/* <!-- Content wrapper --> */}
@@ -336,7 +101,8 @@ function Home() {
                     </div>
                   </div>
                 </div>
-                {/* <!-- Users List Table --> */}
+                {/* <!-- Users List Table --> */} 
+                
                 <div className="card">
                   <div className="card-header border-bottom">
                     <h5 className="card-title">Search Filter</h5>
@@ -351,10 +117,10 @@ function Home() {
                       <thead>
                         <tr>
                           <th></th>
-                          <th>User</th>
-                          <th>Role</th>
-                          <th>Plan</th>
-                          <th>Billing</th>
+                          <th>Avera ID</th>
+                          <th>Test</th>
+                          <th>Cost</th>
+                          <th>Gender</th>
                           <th>Status</th>
                           <th>Actions</th>
                         </tr>
