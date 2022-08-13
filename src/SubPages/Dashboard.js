a983e78766353385d59e427c2ae2367973f2dc26
eben-siaw <edei-siaw@st.ug.edu.gh>
2022-08-13 12:10:35 +0100

    new changes

--- src/SubPages/Dashboard.js
+++ src/SubPages/Dashboard.js
@@ -8,12 +8,12 @@ const PendingBookings = () => {
       <div className="layout-wrapper layout-horizontal layout-without-menu layout-navbar-full"> 
           {/* <!-- Layout container --> */}
         <div className="layout-container">
-          <TabBar/>     
+          {/* <TabBar/>      */}
          
        <div className="layout-page">
-          <Menu/>
+          {/* <Menu/> */}
        
-        <div className="content-wrapper main-session"> 
+        <div className="content-wrapper"> 
          {/* <!--  Pending Bookings --> */} 
 
         <div className="container-xxl flex-grow-1 container-p-y">
@@ -97,230 +97,22 @@ const PendingBookings = () => {
           </div>
         </div> 
             
-           <div className="offcanvas offcanvas-end" id="add-new-record">
-                <div className="offcanvas-header border-bottom">
-                  <h5 className="offcanvas-title" id="exampleModalLabel">New Record</h5>
-                  <button
-                    type="button"
-                    className="btn-close text-reset"
-                    data-bs-dismiss="offcanvas"
-                    aria-label="Close"
-                  ></button>
-                </div>
-                <div className="offcanvas-body flex-grow-1">
-                  <form className="add-new-record pt-0 row g-2" id="form-add-new-record" onsubmit="return false">
-                    <div className="col-sm-12">
-                      <label className="form-label" for="basicFullname">Full Name</label>
-                      <div className="input-group input-group-merge">
-                        <span id="basicFullname2" className="input-group-text"><i className="bx bx-user"></i></span>
-                        <input
-                          type="text"
-                          id="basicFullname"
-                          className="form-control dt-full-name"
-                          name="basicFullname"
-                          placeholder="John Doe"
-                          aria-label="John Doe"
-                          aria-describedby="basicFullname2"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-sm-12">
-                      <label className="form-label" for="basicPost">Post</label>
-                      <div className="input-group input-group-merge">
-                        <span id="basicPost2" className="input-group-text"><i className="bx bxs-briefcase"></i></span>
-                        <input
-                          type="text"
-                          id="basicPost"
-                          name="basicPost"
-                          className="form-control dt-post"
-                          placeholder="Web Developer"
-                          aria-label="Web Developer"
-                          aria-describedby="basicPost2"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-sm-12">
-                      <label className="form-label" for="basicEmail">Email</label>
-                      <div className="input-group input-group-merge">
-                        <span className="input-group-text"><i className="bx bx-envelope"></i></span>
-                        <input
-                          type="text"
-                          id="basicEmail"
-                          name="basicEmail"
-                          className="form-control dt-email"
-                          placeholder="john.doe@example.com"
-                          aria-label="john.doe@example.com"
-                        />
-                      </div>
-                      <div className="form-text">You can use letters, numbers & periods</div>
-                    </div>
-                    <div className="col-sm-12">
-                      <label className="form-label" for="basicDate">Joining Date</label>
-                      <div className="input-group input-group-merge">
-                        <span id="basicDate2" className="input-group-text"><i className="bx bx-calendar"></i></span>
-                        <input
-                          type="text"
-                          className="form-control dt-date"
-                          id="basicDate"
-                          name="basicDate"
-                          aria-describedby="basicDate2"
-                          placeholder="MM/DD/YYYY"
-                          aria-label="MM/DD/YYYY"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-sm-12">
-                      <label className="form-label" for="basicSalary">Salary</label>
-                      <div className="input-group input-group-merge">
-                        <span id="basicSalary2" className="input-group-text"><i className="bx bx-dollar"></i></span>
-                        <input
-                          type="number"
-                          id="basicSalary"
-                          name="basicSalary"
-                          className="form-control dt-salary"
-                          placeholder="12000"
-                          aria-label="12000"
-                          aria-describedby="basicSalary2"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-sm-12">
-                      <button type="submit" className="btn btn-primary data-submit me-sm-3 me-1">Submit</button>
-                      <button type="reset" className="btn btn-outline-secondary" data-bs-dismiss="offcanvas">Cancel</button>
-                    </div>
-                  </form>
-                </div>
-              </div>
-
-        {/* <!-- Users List Table --> */} 
-           <div className="card">
-                <h5 className="card-header">Pending Bookings</h5>
-                <div className="card-datatable table-responsive">
-                  <table className="dt-multilingual table table-bordered">
+          <div class="card">
+                <h5 class="card-header">Pending Bookings</h5>
+                <div class="card-datatable text-nowrap">
+                  <table class="datatables-ajax border table table-bordered">
                     <thead>
                       <tr>
-                        <th></th>
                         <th>Avera ID</th>
                         <th>Test</th>
                         <th>Cost</th>
                         <th>Status</th>
-                        <th>Action</th>
-                      </tr>
-                    </thead>
-                  </table>
-                </div>
-         </div>
-
-        {/* <div className="card">
-                <h5 className="card-header">Pending Bookings</h5>
-                <div className="table-responsive text-nowrap">
-                  <table className="table">
-                    <thead>
-                      <tr>
-                        <th>AVERA ID</th>
-                        <th>TEST</th>
-                        <th>COST</th>
-                        <th>Status</th>
                         <th>Actions</th>
                       </tr>
                     </thead>
-                    <tbody className="table-border-bottom-0">
-                      <tr>
-                        <td><i className="fa fa-user fa-lg text-danger me-3"></i> <strong>AV-33244</strong></td>
-                        <td>Blood test</td>
-                        <td> 
-                        GH₵ 200
-                       
-                        </td>
-                        <td><span className="badge bg-label-primary me-1">Active</span></td>
-                        <td>
-                          <div className="dropdown">
-                            <button type="button" className="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
-                              <i className="bx bx-dots-vertical-rounded"></i>
-                            </button>
-                            <div className="dropdown-menu">
-                              <a className="dropdown-item" href="/viewprofile"
-                                ><i className="bx bx-show me-2"></i> View </a>
-                              <a className="dropdown-item" href="/startprocess"
-                                ><i className="bx bx-send me-1"></i> Start</a
-                              >
-                            </div>
-                          </div>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><i className="fa fa-user fa-lg text-info me-3"></i> <strong>AV-33245</strong></td>
-                        <td>Liver test</td>
-                        <td> 
-                        GH₵ 200
-                         
-                        </td>
-                        <td><span className="badge bg-label-success me-1">Completed</span></td>
-                        <td>
-                          <div className="dropdown">
-                            <button type="button" className="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
-                              <i className="bx bx-dots-vertical-rounded"></i>
-                            </button>
-                            <div className="dropdown-menu">
-                              <a className="dropdown-item" href="/viewprofile"
-                                ><i className="bx bx-show me-2"></i> View </a>
-                              <a className="dropdown-item" href="/startprocess"
-                                ><i className="bx bx-send me-1"></i> Start</a
-                              >
-                            </div>
-                          </div>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><i className="fa fa-user fa-lg text-success me-3"></i> <strong>AV-33246</strong></td>
-                        <td>Hepatitis A</td>
-                        <td>
-                        GH₵ 200
-                        
-                        </td>
-                        <td><span className="badge bg-label-info me-1">Scheduled</span></td>
-                        <td>
-                          <div className="dropdown">
-                            <button type="button" className="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
-                              <i className="bx bx-dots-vertical-rounded"></i>
-                            </button>
-                            <div className="dropdown-menu">
-                              <a className="dropdown-item" href="/viewprofile"
-                                ><i className="bx bx-show me-2"></i> View</a> 
-                                <a className="dropdown-item" href="/startprocess"
-                                ><i className="bx bx-send me-1"></i> Start</a>
-                            </div>
-                          </div>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <i className="fa fa-user fa-lg text-primary me-3"></i> <strong>AV-33247</strong>
-                        </td>
-                        <td>Syphillis</td>
-                        <td> 
-                         GH₵ 200
-                          
-                        </td>
-                        <td><span className="badge bg-label-warning me-1">Pending</span></td>
-                        <td>
-                          <div className="dropdown">
-                            <button type="button" className="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
-                              <i className="bx bx-dots-vertical-rounded"></i>
-                            </button>
-                            <div className="dropdown-menu">
-                              <a className="dropdown-item" href="/viewprofile"
-                                ><i className="bx bx-show"></i> View </a> 
-                                <a className="dropdown-item" href="/startprocess"
-                                ><i className="bx bx-send"></i> Start </a>
-                            </div>
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody>
                   </table>
                 </div>
-        </div> */}
+              </div>
       </div>
       {/* <!-- / Content --> */}
 
